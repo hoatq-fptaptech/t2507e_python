a0a9bd3121d226bacc84493a7d3fd3b31d96e558
quanghoatrinh <quanghoa.trinh@gmail.com>
2026-04-22 18:57:18 +0700

    update code

--- demo4.py
+++ demo4.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,text
 
 db="mysql+pymysql://root:root@localhost:3306/t2507e_jp"
 engine = create_engine(db)
@@ -18,3 +18,18 @@ df2.to_sql(
     if_exists="replace", #append
     index=False
 )
+
+insert_sql = text("INSERT INTO students(name,dob,mark,group_id) values('Tien Anh','2010-09-19',8,1)")
+#conn = engine.connect()
+with engine.connect() as conn:
+    conn.excute(insert_sql)
+    conn.commit()
+
+
+# DB có bảng users(id,name,email,salary)
+# yêu cầu:
+# 1. Tao 1 file users.csv có 20 người theo thông tin trên
+# 2. Nạp dữ liệu vào db- table: users
+# 3. THêm 1 column: net_salary
+# 4. Tính dữ liệu vào net_salary (giar sử net = salary-10%)
+# 5. Xuất dữ liệu mới ra csv
